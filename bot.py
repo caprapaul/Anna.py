1bb0fe12f7895f6e91ca7c069f6e045f331b15c5
Paul Capra <c.paulica@gmail.com>
2019-02-21 12:02:38 +0200

    Added some command error messages

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import sys
+import traceback
 
 # Global
 DESCRIPTION = "Hi! My name is Anna. I am a bot under development by Acey#4962. You can access my commands by typing in \"Anna, \" followed by your command. See you around!"
@@ -25,6 +27,46 @@ async def on_message(message):
     output = f'{message.author} says: {message.content}'
     print(output)
     await bot.process_commands(message)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    """The event triggered when an error is raised while invoking a command.
+    ctx   : Context
+    error : Exception"""
+    # This prevents any commands with local handlers being handled here in on_command_error.
+    if hasattr(ctx.command, 'on_error'):
+        return
+
+    ignored = (commands.UserInputError, commands.BadArgument)
+
+    # Allows us to check for original exceptions raised and sent to CommandInvokeError.
+    # If nothing is found. We keep the exception passed to on_command_error.
+    error = getattr(error, 'original', error)
+
+    # Anything in ignored will return and prevent anything happening.
+    if isinstance(error, ignored):
+        return
+
+    elif isinstance(error, commands.CommandNotFound):
+        return await ctx.send('Invalid command', delete_after=10)
+
+    elif isinstance(error, commands.DisabledCommand):
+        return await ctx.send(f'{ctx.command} has been disabled.', delete_after=10)
+
+    elif isinstance(error, commands.NoPrivateMessage):
+        try:
+            return await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
+        except:
+            pass
+    elif isinstance(error, commands.NotOwner):
+        return await ctx.send("You do not own this bot.", delete_after=10)
+    elif isinstance(error, commands.MissingPermissions):
+        return await ctx.send(error.message, delete_after=10)
+
+    # All other Errors not returned come here... And we can just print the default TraceBack.
+    print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+    traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
 # <------ Commands ------>
 @bot.command()
